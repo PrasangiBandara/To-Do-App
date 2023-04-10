d3c93790ebce0876749a4ec3fadf4f9a113cac3d
PrasangiBandara <mail2prasangi@gmail.com>
2023-04-10 14:10:40 +0530

    implement index.ts file to do all the functions in to do app

--- index.js
+++ index.js
@@ -2,5 +2,87 @@ var Todo = /** @class */ (function () {
     function Todo() {
         this.task = [];
     }
+    //add task
+    Todo.prototype.addToList = function (taskName) {
+        var task = {
+            name: taskName,
+            completed: false,
+        };
+        this.task.push(task);
+        this.functioningTask();
+    };
+    //edit task
+    Todo.prototype.editDoList = function (index, taskName) {
+        this.task[index].name = taskName;
+        this.functioningTask();
+    };
+    //delete task
+    Todo.prototype.deleteToDoList = function (index) {
+        this.task.splice(index, 1);
+        this.functioningTask();
+    };
+    //tick complete task
+    Todo.prototype.completeTask = function (index) {
+        this.task[index].completed = !this.task[index].completed;
+        this.functioningTask();
+    };
+    //functions for tasks
+    Todo.prototype.functioningTask = function () {
+        var _this = this;
+        var doList = document.getElementById("list");
+        var allAboutTasks = document.getElementById("all");
+        var count = 0;
+        doList.innerHTML = "";
+        this.task.forEach(function (task, index) {
+            var listItem = document.createElement("li");
+            listItem.className = "task-item";
+            if (task.completed) {
+                listItem.classList.add("completed");
+                count++;
+            }
+            var checkbox = document.createElement("input");
+            checkbox.type = "checkbox";
+            checkbox.checked = task.completed;
+            checkbox.addEventListener("change", function () {
+                _this.completeTask(index);
+            });
+            var taskName = document.createElement("span");
+            taskName.textContent = task.name;
+            taskName.className = "task-name";
+            var editButton = document.createElement("button");
+            editButton.textContent = "Edit";
+            editButton.className = "edit-button";
+            editButton.addEventListener("click", function () {
+                var newTaskName = prompt("Edit Your Task :", task.name);
+                if (newTaskName) {
+                    _this.editDoList(index, newTaskName);
+                }
+            });
+            var deleteButton = document.createElement("button");
+            deleteButton.textContent = "Delete";
+            deleteButton.className = "delete-button";
+            deleteButton.addEventListener("click", function () {
+                _this.deleteToDoList(index);
+            });
+            listItem.appendChild(checkbox);
+            listItem.appendChild(taskName);
+            listItem.appendChild(editButton);
+            listItem.appendChild(deleteButton);
+            doList.appendChild(listItem);
+        });
+        var Total = this.task.length;
+        var Incomplete = Total - count;
+        allAboutTasks.innerHTML = "<p>Total tasks: <span>".concat(Total, "</span></p>\n                               <p>Completed tasks: <span>").concat(count, "</span></p>\n                               <p>Incomplete tasks: <span>").concat(Incomplete, "</span></p>");
+    };
     return Todo;
 }());
+var todo = new Todo();
+var Input = document.getElementById("taskInput");
+var addButton = document.getElementById("add-button");
+addButton.addEventListener("click", function () {
+    var taskName = Input.value.trim();
+    if (taskName) {
+        todo.addToList(taskName);
+        Input.value = "";
+    }
+});
--- index.ts
+++ index.ts
@@ -9,4 +9,103 @@ class Todo {
   constructor() {
     this.task = [];
   }
+
+  //add task
+  addToList(taskName: string): void {
+    const task: ToDo = {
+      name: taskName,
+      completed: false,
+    };
+    this.task.push(task);
+    this.functioningTask();
+  }
+
+  //edit task
+  editDoList(index: number, taskName: string): void {
+    this.task[index].name = taskName;
+    this.functioningTask();
+  }
+
+  //delete task
+  deleteToDoList(index: number): void {
+    this.task.splice(index, 1);
+    this.functioningTask();
+  }
+
+  //tick complete task
+  completeTask(index: number): void {
+    this.task[index].completed = !this.task[index].completed;
+    this.functioningTask();
+  }
+
+  //functions for tasks
+  functioningTask(): void {
+    const doList = document.getElementById("list");
+    const allAboutTasks = document.getElementById("all");
+    let count = 0;
+
+    doList.innerHTML = "";
+
+    this.task.forEach((task: ToDo, index: number) => {
+      const listItem = document.createElement("li");
+      listItem.className = "task-item";
+      if (task.completed) {
+        listItem.classList.add("completed");
+        count++;
+      }
+
+      const checkbox = document.createElement("input");
+      checkbox.type = "checkbox";
+      checkbox.checked = task.completed;
+      checkbox.addEventListener("change", () => {
+        this.completeTask(index);
+      });
+
+      const taskName = document.createElement("span");
+      taskName.textContent = task.name;
+      taskName.className = "task-name";
+
+      const editButton = document.createElement("button");
+      editButton.textContent = "Edit";
+      editButton.className = "edit-button";
+      editButton.addEventListener("click", () => {
+        const newTaskName = prompt("Edit Your Task :", task.name);
+        if (newTaskName) {
+          this.editDoList(index, newTaskName);
+        }
+      });
+
+      const deleteButton = document.createElement("button");
+      deleteButton.textContent = "Delete";
+      deleteButton.className = "delete-button";
+      deleteButton.addEventListener("click", () => {
+        this.deleteToDoList(index);
+      });
+
+      listItem.appendChild(checkbox);
+      listItem.appendChild(taskName);
+      listItem.appendChild(editButton);
+      listItem.appendChild(deleteButton);
+      doList.appendChild(listItem);
+    });
+
+    const Total = this.task.length;
+    const Incomplete = Total - count;
+
+    allAboutTasks.innerHTML = `<p>Total tasks: <span>${Total}</span></p>
+                               <p>Completed tasks: <span>${count}</span></p>
+                               <p>Incomplete tasks: <span>${Incomplete}</span></p>`;
+  }
 }
+
+const todo = new Todo();
+
+const Input = document.getElementById("taskInput") as HTMLInputElement;
+const addButton = document.getElementById("add-button");
+addButton.addEventListener("click", () => {
+  const taskName = Input.value.trim();
+  if (taskName) {
+    todo.addToList(taskName);
+    Input.value = "";
+  }
+});
